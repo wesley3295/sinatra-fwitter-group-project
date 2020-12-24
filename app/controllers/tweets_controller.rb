e53cce75892a9b09dc893db1895b7fc4d5b64065
Wesley <wspencer3295@yahoo.com>
2020-12-24 10:55:01 -0700

    fwitter migrate error

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,17 @@
 class TweetsController < ApplicationController
 
+    get '/tweets' do
+        @current_user = User.find_by_id(session[:user_id])
+        if @current_user
+         erb :'/tweets/index' 
+        else
+            erb :'/tweets/error' 
+        end
+        
+    end
+
+    get '/tweets/new' do
+        erb :'/tweets/new'
+    end
 
 end
